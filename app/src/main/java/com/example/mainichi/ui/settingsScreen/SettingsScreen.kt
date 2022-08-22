cdffbe27d55955c74c7c1a5bb269f3ad9b155191
Benjamin Kunz <bkunz.mmt-b2020@fh-salzburg.ac.at>
2022-08-22 17:02:15 +0200

    save Notification button

--- app/src/main/java/com/example/mainichi/ui/settingsScreen/SettingsScreen.kt
+++ app/src/main/java/com/example/mainichi/ui/settingsScreen/SettingsScreen.kt
@@ -1,20 +1,17 @@
 package com.example.mainichi.ui.settingsScreen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateMap
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -43,12 +40,17 @@ fun SettingsScreen(
 
 ) {
 
-    Column(modifier = Modifier.fillMaxWidth()) {
-        Text("Create new Notification")
+    Column(modifier = Modifier.fillMaxWidth(),
+    verticalArrangement = Arrangement.spacedBy(24.dp)) {
 
-        var selectedAssetMap = remember { mutableStateMapOf<String, Boolean>() }
-        var selectedEventMap = remember { mutableStateMapOf<String, Boolean>() }
-        var selectedIntervalMap = remember { mutableStateMapOf<String, Boolean>() }
+        Text(
+            text = "Create new Notification",
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        val selectedAssetMap = remember { mutableStateMapOf<String, Boolean>() }
+        val selectedEventMap = remember { mutableStateMapOf<String, Boolean>() }
+        val selectedIntervalMap = remember { mutableStateMapOf<String, Boolean>() }
 
         val content = listOf("Bitcoin", "Ethereum", "Cardano")
 
@@ -58,14 +60,56 @@ fun SettingsScreen(
             selectedIntervalMap[it] = false
         }
 
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()
+
+        SelectableDropDown(
+            title = "Asset",
+            content = content,
+            selectedMap = selectedAssetMap,
+            onSelect = { name ->
+
+                remapSelection(selectedAssetMap, name)
+            })
+
+        SelectableDropDown(
+            title = "Event",
+            content = content,
+            selectedMap = selectedEventMap,
+            onSelect = { name ->
+                remapSelection(selectedEventMap, name)
+            }
+        )
+//
+            SelectableDropDown(
+                title = "Interval",
+                content = content,
+                selectedMap = selectedIntervalMap,
+                onSelect = { name ->
+                    remapSelection(selectedIntervalMap, name)
+                }
+            )
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .size(100.dp, 60.dp)
+                .padding(top = 16.dp)
         ) {
-            SelectableDropDown("Asset", content, selectedAssetMap)
+            Text(text = "Save")
+        }
+    }
+}
 
-            SelectableDropDown("Event", content, selectedEventMap)
 
-            SelectableDropDown("Interval", content, selectedIntervalMap)
+private fun remapSelection(
+    selectedAssetMap: SnapshotStateMap<String, Boolean>,
+    name: String
+) {
+    selectedAssetMap.forEach { (s, _) ->
+
+        if (s == name) {
+            selectedAssetMap[name] = true
+        } else {
+            selectedAssetMap[s] = false
         }
     }
 }
@@ -74,51 +118,71 @@ fun SettingsScreen(
 fun SelectableDropDown(
     title: String,
     content: List<String>,
-    selectedMap: SnapshotStateMap<String, Boolean>
+    selectedMap: SnapshotStateMap<String, Boolean>,
+    onSelect: (String) -> Unit
 ) {
 
-    Column() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
 
         var expanded by remember { mutableStateOf(false) }
-        var asset by remember { mutableStateOf("") }
+        var selectedAsset by remember { mutableStateOf("") }
 
-        Text(text = title, modifier = Modifier.clickable {
-            expanded = !expanded
-        })
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .background(MaterialTheme.colors.onBackground),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-        Text(text = asset)
+            Text(
+                text = title,
+                color = MaterialTheme.colors.secondaryVariant,
+                modifier = Modifier
+                    .clickable {
+                        expanded = !expanded
+                    })
 
-        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
 
-            content.forEach { name ->
+                content.forEach { name ->
 
-                DropdownMenuItem(onClick = {
+                    DropdownMenuItem(onClick = {
 
-                    selectedMap.forEach { (s, _) ->
+                        onSelect(name)
+                        selectedAsset = name
 
-                        if (s == name) {
-                            selectedMap[name] = true
-                        } else {
-                            selectedMap[s] = false
+                    }) {
+
+                        Row() {
+                            Icon(
+                                imageVector = if (selectedMap[name] == true) {
+                                    Icons.Filled.CheckCircle
+                                } else {
+                                    Icons.Default.Check
+                                }, contentDescription = null
+                            )
+
+                            Text(text = name)
                         }
-                    }
-
-                    asset = name
-                }) {
-
-                    Row() {
-                        Icon(
-                            imageVector = if (selectedMap[name] == true) {
-                                Icons.Filled.CheckCircle
-                            } else {
-                                Icons.Default.Check
-                            }, contentDescription = null
-                        )
-
-                        Text(text = name)
                     }
                 }
             }
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colors.onBackground),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = selectedAsset,
+                color = MaterialTheme.colors.secondaryVariant
+            )
         }
     }
 }
